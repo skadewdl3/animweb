80bfc25ac9cad8a2cb6d10366975cd0abbd56e15
Soham K <sohamk10@gmail.com>
2023-03-20 12:33:20 +0530

    Added Observables.x and Observables.y support to text

--- src/animweb/AnimObjects/Point.ts
+++ src/animweb/AnimObjects/Point.ts
@@ -1,35 +1,40 @@
-import { evaluate, round } from 'mathjs'
-import p5 from 'p5'
-import { v4 as uuid } from 'uuid'
-import AnimObject, { AnimObjectProps, AnimObjects } from '../AnimObject'
-import Color from '../helpers/Color'
-import { roundOff } from '../helpers/miscellaneous'
-import { rangePerFrame } from './../helpers/miscellaneous'
+import { evaluate, round } from "mathjs";
+import p5 from "p5";
+import { v4 as uuid } from "uuid";
+import AnimObject, {
+  AnimObjectProps,
+  AnimObjects,
+  Observables,
+  Observer,
+} from "../AnimObject";
+import Color from "../helpers/Color";
+import { roundOff } from "../helpers/miscellaneous";
+import { rangePerFrame } from "./../helpers/miscellaneous";
 
 export interface PointProps extends AnimObjectProps {
-  x: number
-  y: number
-  z?: number
-  size?: number
-  definition?: string
+  x: number;
+  y: number;
+  z?: number;
+  size?: number;
+  definition?: string;
   parentData?: {
-    origin: { x: number; y: number }
-    stepX: number
-    stepY: number
-  }
+    origin: { x: number; y: number };
+    stepX: number;
+    stepY: number;
+  };
 }
 
 class Point extends AnimObject {
-  x: number
-  y: number
-  z?: number
-  size: number
-  definition: string = ''
+  x: number;
+  y: number;
+  z?: number;
+  size: number;
+  definition: string = "";
   parentData: {
-    origin: { x: number; y: number }
-    stepX: number
-    stepY: number
-  } = { origin: { x: 0, y: 0 }, stepX: 1, stepY: 1 }
+    origin: { x: number; y: number };
+    stepX: number;
+    stepY: number;
+  } = { origin: { x: 0, y: 0 }, stepX: 1, stepY: 1 };
 
   constructor({
     x,
@@ -40,81 +45,121 @@ class Point extends AnimObject {
     definition,
     parentData,
   }: PointProps) {
-    super()
-    this.x = x
-    this.y = y
-    this.z = z
-    this.size = size
-    if (definition) this.definition = definition
-    if (color) this.color = color
+    super();
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.size = size;
+    if (definition) this.definition = definition;
+    if (color) this.color = color;
     if (this.parentData) {
       this.parentData = {
         ...this.parentData,
         ...parentData,
-      }
+      };
+    }
+  }
+
+  setX(x: number) {
+    this.x = x;
+    this.observers.forEach((observer: Observer) => {
+      if (observer.property == Observables.x)
+        observer.handler(
+          (this.x - this.parentData.origin.x) / this.parentData.stepX
+        );
+    });
+  }
+
+  setY(y: number) {
+    this.y = y;
+    this.observers.forEach((observer: Observer) => {
+      if (observer.property == Observables.y)
+        observer.handler(
+          (this.parentData.origin.y - this.y) / this.parentData.stepY
+        );
+    });
+  }
+
+  addObserver(observer: Observer): void {
+    this.observers.push(observer);
+    switch (observer.property) {
+      case Observables.x:
+        observer.handler(
+          (this.x - this.parentData.origin.x) / this.parentData.stepX
+        );
+        break;
+      case Observables.y:
+        observer.handler(
+          (this.parentData.origin.y - this.y) / this.parentData.stepY
+        );
+        break;
     }
   }
 
   moveTo({ x, duration = 1 }: { x: number; duration?: number }): Promise<void> {
     let transitionQueueItem = {
       id: uuid(),
-    }
-    let queued = false
+    };
+    let queued = false;
     return new Promise((resolve, reject) => {
-      let y = evaluate(this.definition, { x })
-      let newX = this.parentData.origin.x + x * this.parentData.stepX
-      let newY = this.parentData.origin.y - y * this.parentData.stepY
-      let speed = rangePerFrame(Math.abs(this.x - newX), duration)
+      let y = evaluate(this.definition, { x });
+      let newX = this.parentData.origin.x + x * this.parentData.stepX;
+      let newY = this.parentData.origin.y - y * this.parentData.stepY;
+      let speed = rangePerFrame(Math.abs(this.x - newX), duration);
       this.transition = () => {
-        this.queueTransition(transitionQueueItem)
-        queued = true
+        this.queueTransition(transitionQueueItem);
+        queued = true;
         if (roundOff(this.x, 0) < roundOff(newX, 0)) {
           if (this.x + speed > newX) {
-            this.x = newX
-            this.y = newY
-            this.transition = null
-            this.unqueueTransition(transitionQueueItem)
-            resolve()
+            this.setX(newX);
+            this.setY(newY);
+            this.transition = null;
+            this.unqueueTransition(transitionQueueItem);
+            resolve();
           } else {
-            this.x += speed
-            this.y =
-              this.parentData.origin.y +
-              evaluate(this.definition, {
-                x: (this.parentData.origin.x - this.x) / this.parentData.stepX,
-              }) *
-                this.parentData.stepY
+            this.setX(this.x + speed);
+            this.setY(
+              this.parentData.origin.y -
+                evaluate(this.definition, {
+                  x:
+                    (this.x - this.parentData.origin.x) / this.parentData.stepX,
+                }) *
+                  this.parentData.stepY
+            );
           }
         }
         if (roundOff(this.x, 0) > roundOff(newX, 0)) {
           if (this.x - speed < newX) {
-            this.x = newX
-            this.y = newY
-            this.transition = null
-            this.unqueueTransition(transitionQueueItem)
-            resolve()
+            this.setX(newX);
+            this.setY(newY);
+            this.transition = null;
+            this.unqueueTransition(transitionQueueItem);
+            resolve();
           } else {
-            this.x -= speed
-            this.y =
-              this.parentData.origin.y +
-              evaluate(this.definition, {
-                x: (this.parentData.origin.x - this.x) / this.parentData.stepX,
-              }) *
-                this.parentData.stepY
+            this.setX(this.x - speed);
+            this.setY(
+              this.parentData.origin.y -
+                evaluate(this.definition, {
+                  x:
+                    (this.x - this.parentData.origin.x) / this.parentData.stepX,
+                }) *
+                  this.parentData.stepY
+            );
           }
         }
-      }
-    })
+      };
+    });
   }
 
   draw(p: p5) {
     if (this.transition) {
-      this.transition()
+      this.transition();
     }
-    p.stroke(this.color.rgba)
-    p.strokeWeight(this.size)
-    p.point(this.x, this.y)
-    p.noStroke()
+    p.stroke(this.color.rgba);
+    p.strokeWeight(this.size);
+    p.point(this.x, this.y);
+    p.noStroke();
   }
 }
 
-export default Point
+export default Point;
--- src/animweb/AnimObjects/Text.ts
+++ src/animweb/AnimObjects/Text.ts
@@ -1,24 +1,24 @@
-import p5 from 'p5'
-import AnimObject, { AnimObjectProps } from '../AnimObject'
-import StandardColors from '../helpers/StandardColors'
-import { Observables } from '../AnimObject'
-import { roundOff } from '../helpers/miscellaneous'
-import Color from '../helpers/Color'
+import p5 from "p5";
+import AnimObject, { AnimObjectProps } from "../AnimObject";
+import StandardColors from "../helpers/StandardColors";
+import { Observables } from "../AnimObject";
+import { roundOff } from "../helpers/miscellaneous";
+import Color from "../helpers/Color";
 
 interface TextProps extends AnimObjectProps {
-  text?: string | number
-  color?: Color
-  size?: number
+  text?: string | number;
+  color?: Color;
+  size?: number;
   position?: {
-    x: number
-    y: number
-  }
-  style?: TextStyle
+    x: number;
+    y: number;
+  };
+  style?: TextStyle;
   parentData?: {
-    origin?: { x: number; y: number }
-    stepX?: number
-    stepY?: number
-  }
+    origin?: { x: number; y: number };
+    stepX?: number;
+    stepY?: number;
+  };
 }
 
 export enum TextStyle {
@@ -29,77 +29,85 @@ export enum TextStyle {
 }
 
 export default class Text extends AnimObject {
-  text: string | number = ''
-  size: number = 16
-  x: number
-  y: number
+  text: string | number = "";
+  size: number = 16;
+  x: number;
+  y: number;
   parentData: {
-    origin: { x: number; y: number }
-    stepX: number
-    stepY: number
+    origin: { x: number; y: number };
+    stepX: number;
+    stepY: number;
   } = {
     origin: { x: this.sceneWidth / 2, y: this.sceneHeight / 2 },
     stepX: 1,
     stepY: 1,
-  }
-  style: TextStyle = TextStyle.none
+  };
+  style: TextStyle = TextStyle.none;
 
   constructor(config: TextProps = {}) {
-    super()
-    if (config.text) this.text = config.text
-    if (config.color) this.color = config.color
-    if (config.size) this.size = config.size
+    super();
+    if (config.text) this.text = config.text;
+    if (config.color) this.color = config.color;
+    if (config.size) this.size = config.size;
     if (config.style) {
-      this.style = config.style
+      this.style = config.style;
     }
     if (config.parentData) {
       this.parentData = {
         ...this.parentData,
         ...config.parentData,
-      }
+      };
     }
-    this.x = this.parentData.origin.x
-    this.y = this.parentData.origin.y
+    this.x = this.parentData.origin.x;
+    this.y = this.parentData.origin.y;
     if (config.position) {
-      this.x += config.position.x * this.parentData.stepX
-      this.y -= config.position.y * this.parentData.stepY
+      this.x += config.position.x * this.parentData.stepX;
+      this.y -= config.position.y * this.parentData.stepY;
     }
   }
 
   link(object: AnimObject, prop: Observables) {
-    if (!(prop in object)) return
+    if (!(prop in object)) return;
 
     let observer = {
       property: prop,
       handler: (text: string) => {
         switch (prop) {
           case Observables.slope:
-            this.text = `Slope: ${roundOff(parseFloat(text), 3)}`
-            break
+            this.text = `Slope: ${roundOff(parseFloat(text), 3)}`;
+            break;
+          case Observables.x:
+            this.text = `x: ${roundOff(parseFloat(text), 3)}`;
+            break;
+          case Observables.y:
+            this.text = `y: ${roundOff(parseFloat(text), 3)}`;
+            console.log(text);
+            break;
           default:
-            this.text = text
+            this.text = text;
+            break;
         }
       },
-    }
+    };
 
-    object.addObserver(observer)
+    object.addObserver(observer);
   }
 
   draw(p: p5) {
-    p.fill(this.color.rgba)
-    p.textSize(this.size)
+    p.fill(this.color.rgba);
+    p.textSize(this.size);
     switch (this.style) {
       case TextStyle.bold:
-        p.textStyle(p.BOLD)
-        break
+        p.textStyle(p.BOLD);
+        break;
       case TextStyle.italic:
-        p.textStyle(p.ITALIC)
-        break
+        p.textStyle(p.ITALIC);
+        break;
       case TextStyle.boldItalic:
-        p.textStyle(p.BOLDITALIC)
-        break
+        p.textStyle(p.BOLDITALIC);
+        break;
     }
-    p.text(this.text, this.x, this.y)
-    p.noFill()
+    p.text(this.text, this.x, this.y);
+    p.noFill();
   }
 }
