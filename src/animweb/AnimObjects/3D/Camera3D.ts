88194197a3c0ab405f95313108a7d93f08b2e99a
Soham K <sohamk10@gmail.com>
2023-04-26 22:00:24 +0530

    added a bunch of rotation stuff for camera3d

--- src/animweb/AnimObjects/3D/Camera3D.ts
+++ src/animweb/AnimObjects/3D/Camera3D.ts
@@ -12,6 +12,35 @@ export default class Camera {
   camera: THREE.PerspectiveCamera
   transitions: Array<{ id: string; function: Function }> = []
   origin: { x: number; y: number; z: number }
+  rotationStopCondition: Function = () => false
+  rotationTransition: {
+    id: string
+    function: Function
+    rotation: Function
+    stopCondition: Function
+    originalX: number
+    originalY: number
+    originalZ: number
+  } = {
+    id: uuid(),
+    rotation: () => {},
+    stopCondition: () => false,
+    function: () => {
+      if (!this.rotationStopCondition()) this.rotationTransition.rotation()
+      else {
+        this.transitions = this.transitions.filter(
+          (transition) => transition.id != this.rotationTransition.id
+        )
+        this.camera.position.x = this.rotationTransition.originalX
+        this.camera.position.y = this.rotationTransition.originalY
+        this.camera.position.z = this.rotationTransition.originalZ
+        this.rotationStopCondition = () => false
+      }
+    },
+    originalX: 0,
+    originalY: 0,
+    originalZ: 0,
+  }
 
   constructor(width: number, height: number) {
     this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
@@ -21,6 +50,9 @@ export default class Camera {
       y: 0,
       z: 0,
     }
+    this.rotationTransition.originalX = this.camera.position.x
+    this.rotationTransition.originalY = this.camera.position.y
+    this.rotationTransition.originalZ = this.camera.position.z
     this.lookAtWithoutTransition(this.origin.x, this.origin.y, this.origin.z)
   }
 
@@ -62,9 +94,73 @@ export default class Camera {
     this.camera.lookAt(this.origin.x, this.origin.y, this.origin.z)
   }
 
-  startRotation() {}
+  startRotation() {
+    if (this.rotationTransition) this.transitions.push(this.rotationTransition)
+  }
 
-  stopRotation() {}
+  stopRotation() {
+    if (this.rotationTransition) {
+      this.rotationStopCondition = () => {
+        return (
+          roundOff(this.rotationTransition.originalX, 0) ==
+            roundOff(this.camera.position.x, 0) &&
+          roundOff(this.rotationTransition.originalY, 0) ==
+            roundOff(this.camera.position.y, 0) &&
+          roundOff(this.rotationTransition.originalZ, 0) ==
+            roundOff(this.camera.position.z, 0)
+        )
+      }
+    }
+  }
+
+  rotate() {
+    let radius = Math.sqrt(
+      this.camera.position.z ** 2 + this.camera.position.x ** 2
+    )
+    let angle = Math.atan(this.camera.position.z / this.camera.position.x)
+    this.rotationTransition.originalX = this.camera.position.x
+    this.rotationTransition.originalY = this.camera.position.y
+    this.rotationTransition.originalZ = this.camera.position.z
+    this.rotationTransition.rotation = () => {
+      angle += 0.1
+      this.camera.position.x = radius * Math.cos(angle)
+      this.camera.position.z = radius * Math.sin(angle)
+      this.lookAtWithoutTransition(this.origin.x, this.origin.y, this.origin.z)
+    }
+    this.startRotation()
+  }
+  // rotateYZ() {
+  //   let radius = Math.sqrt(
+  //     this.camera.position.z ** 2 + this.camera.position.y ** 2
+  //   )
+  //   let angle = Math.atan(this.camera.position.z / this.camera.position.y)
+  //   this.rotationTransition.originalX = this.camera.position.x
+  //   this.rotationTransition.originalY = this.camera.position.y
+  //   this.rotationTransition.originalZ = this.camera.position.z
+  //   this.rotationTransition.rotation = () => {
+  //     angle += 0.1
+  //     this.camera.position.y = radius * Math.cos(angle)
+  //     this.camera.position.z = radius * Math.sin(angle)
+  //     this.lookAtWithoutTransition(this.origin.x, this.origin.y, this.origin.z)
+  //   }
+  //   this.startRotation()
+  // }
+  // rotateXZ() {
+  //   let radius = Math.sqrt(
+  //     this.camera.position.x ** 2 + this.camera.position.y ** 2
+  //   )
+  //   let angle = Math.atan(this.camera.position.y / this.camera.position.x)
+  //   this.rotationTransition.originalX = this.camera.position.x
+  //   this.rotationTransition.originalY = this.camera.position.y
+  //   this.rotationTransition.originalZ = this.camera.position.z
+  //   this.rotationTransition.rotation = () => {
+  //     angle += 0.1
+  //     this.camera.position.x = radius * Math.cos(angle)
+  //     this.camera.position.y = radius * Math.sin(angle)
+  //     this.lookAtWithoutTransition(this.origin.x, this.origin.y, this.origin.z)
+  //   }
+  //   this.startRotation()
+  // }
 
   moveTo(x: number, y: number, z: number, duration: number = 1) {
     let moveToTransition = this.createTransition()
@@ -115,6 +211,15 @@ export default class Camera {
     this.camera.position.x = 0
     this.camera.position.y = 0
     this.camera.position.z = 5
+    this.rotationTransition = {
+      ...this.rotationTransition,
+      id: uuid(),
+      originalX: 0,
+      originalY: 0,
+      originalZ: 0,
+      rotation: () => {},
+      stopCondition: () => false,
+    }
     this.lookAtWithoutTransition(0, 0, 0)
   }
 }
