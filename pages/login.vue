6bd75e75d4b9b41470659cd1c0d64319e399842a
Soham K <sohamk10@gmail.com>
2023-05-20 13:00:09 +0530

    Started working on login/register UI

--- pages/login.vue
+++ pages/login.vue
@@ -57,23 +57,26 @@ const logout = async () => {
     <Title>Animweb - Login</Title>
   </Head>
 <span>
-  <div class="auth-ui">
-
-    <div class="register-ui" :class="{'go-left': mode == 'login'}">
-      <input v-model="username" type="text" placeholder="username">
-      <input v-model="email" type="text" placeholder="email">
-      <input v-model="password" type="password" placeholder="password">
-      <button class="register-btn register-email" @click="registerWithEmail">Register</button>
-      <span>OR</span>
-      <button class="register-btn register-google" @click="registerWithGoogle">Register with google</button>
-    </div>
-    <div class="login-ui" :class="{'go-right': mode == 'register'}">
-      <input type="text" placeholder="username or email">
-      <input v-model="password" type="password" placeholder="password">
-      <button class="login-btn login-email" @click="loginWithEmail">Login</button>
-      <span>OR</span>
-      <button class="login-btn login-google" @click="loginWithGoogle">Login with google</button>
-      <button class="logout-btn" @click="logout">Logout</button>
+  <div class="auth-ui-wrapper">
+    <div class="auth-ui">
+      <div class="register-ui" :class="{'go-left': mode == 'login'}">
+        <input class="auth-ui-input" v-model="username" type="text" placeholder="username">
+        <input class="auth-ui-input" v-model="email" type="text" placeholder="email">
+        <input class="auth-ui-input" v-model="password" type="password" placeholder="password">
+        <button class="register-btn register-email" @click="registerWithEmail">Register</button>
+        <span>OR</span>
+        <button class="register-btn register-google" @click="registerWithGoogle">Register with google</button>
+        <span @click="mode = 'login'">Already have an account ? Login</span>
+      </div>
+      <div class="login-ui" :class="{'go-right': mode == 'register'}">
+        <input class="auth-ui-input" type="text" placeholder="username or email">
+        <input class="auth-ui-input" v-model="password" type="password" placeholder="password">
+        <button class="login-btn login-email" @click="loginWithEmail">Login</button>
+        <span>OR</span>
+        <button class="login-btn login-google" @click="loginWithGoogle">Login with google</button>
+        <span @click="mode = 'register'">Don't have an account ? Register</span>
+      </div>
+      <!-- <button class="logout-btn" @click="logout">Logout</button> -->
     </div>
   </div>
 </span>
@@ -88,5 +91,43 @@ const logout = async () => {
   font-size 10rem
 
 .auth-ui
-  background red
+  position relative
+  width 100%
+  height 20rem
+  overflow hidden
+  &-wrapper
+    // background red
+    width 40rem
+    position absolute
+    top 50%
+    left 50%
+    transform translate(-50%, -50%)
+
+  &-input
+    background #fff
+    border solid 0.1rem #ccc
+    display block
+    border-radius 0.5rem
+    font-size 1.6rem
+    padding 0.5rem 1rem
+    width 100%
+
+.register-ui
+.login-ui
+  position absolute
+  top 0
+  left 0
+  width 100%
+  transition all .2s ease-in-out
+  display flex
+  align-items center
+  justify-content center
+  flex-direction column  
+  
+
+.go-right
+  transform translateX(-200%)
+
+.go-left 
+  transform translateX(200%)
 </style>
