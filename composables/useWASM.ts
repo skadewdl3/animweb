f3c72368f29a4afe7636903431eaf1ff0d1c11ff
Soham K <sohamk10@gmail.com>
2023-06-04 21:18:23 +0530

    did a thing

--- composables/useWASM.ts
+++ composables/useWASM.ts
@@ -2,7 +2,7 @@
 import init, { greet, himom, add, sub } from './../wasm/pkg/wasm.js'
 
 export default async () => {
-  await init('@/public/wasm_bg.wasm')
+  await init('/wasm_bg.wasm')
   return {
     greet,
     himom,
