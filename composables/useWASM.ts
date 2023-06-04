c570d25dce0b66db52d9a9ce1692c948ac49582f
Soham K <sohamk10@gmail.com>
2023-06-04 17:38:30 +0530

    tried another thing

--- composables/useWASM.ts
+++ composables/useWASM.ts
@@ -1,10 +1,8 @@
 // @ts-ignore
-import '@/wasm/pkg/wasm_bg.wasm?init'
 import init, { greet } from 'wasm'
 
-
 export default async () => {
-  await init()
+  await init(await fetch('wasm_bg.wasm'))
   return {
     greet,
   }
